f4d53742599aa1f105cae28c88acd5127781082d
Austin Graves <austingraves106@aol.com>
2019-08-27 12:38:37 -0500

    Setup the initial UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,15 @@
 const apiKey = 'nUT3pXgTT8JuRHnl2o3Pv6aYDfhTH8cJ';
 const apiURL = `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${apiKey}&limit=5`;
 // -----------------------------------------------
+
+let topics = ['Tennessee', 'Florida', 'Colorado', 'North Carolina'];
+
+
+function queryTheAPI() {
+  let options = {
+    url: apiURL,
+    method: 'GET'
+  }
+  $.ajax(options).then(response)
+    console.log(response);
+}
