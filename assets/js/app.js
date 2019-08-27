79890535ea8b4a43cb10a196268d22d901c80f53
Austin Graves <austingraves106@aol.com>
2019-08-27 09:07:06 -0500

    Initial File Structure and got the API Key and URL variables set.

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+// saving the API Key ---------------------------
+const apiKey = 'nUT3pXgTT8JuRHnl2o3Pv6aYDfhTH8cJ';
+const apiURL = `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${apiKey}&limit=5`;
+// -----------------------------------------------
